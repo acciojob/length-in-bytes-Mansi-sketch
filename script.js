91ce7afbf40a43e89fe2a281705e8d6b7ff8e70f
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-01-21 11:08:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const byteSize = (str) => {
   // write your code here
+var blob = new Blob([str], {type: 'text/plain'});
+	return blob.size;
 };
-
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
